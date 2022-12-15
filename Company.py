a4dbd73e86fe24929e794a686b0da7886d482a8a
Afroz <109797079+AfrozPython@users.noreply.github.com>
2022-12-16 04:04:22 +0530

    Add files via upload

---
+++ Company.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 20:29:34 2022
+
+@author: Afroz
+"""
+
+import streamlit as st
+import pickle
+import numpy as np
+
+# import the model
+pipe = pickle.load(open('C:/Users/Afroz/Desktop/Streamlit Test/Best Code Streamlit/Z Test Programs/1000 companies/pipe.pkl','rb'))
+df = pickle.load(open('C:/Users/Afroz/Desktop/Streamlit Test/Best Code Streamlit/Z Test Programs/1000 companies/df.pkl','rb'))
+
+st.title("Company Profit")
+
+
+Research = st.number_input('R&D Spend')                         
+
+Administration = st.number_input('Administration')                                        
+ 
+Marketing = st.number_input('Marketing')                             
+
+State = st.selectbox('State',df['State'].unique())
+
+
+if st.button('Predict Price'):
+    query = np.array([Research,Administration,Marketing,State])
+
+    query = query.reshape(1,4)
+    st.title("The Profit of the company is " + str(int(np.exp(pipe.predict(query)[0]))))
+ 
+ 
+ 
+ 
